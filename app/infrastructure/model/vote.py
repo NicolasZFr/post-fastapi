b20906369788e6c3e5831a7b5b52d9ab9ef16666
NicolasZF <102821750+NicolasZFr@users.noreply.github.com>
2025-04-22 19:18:13 -0500

    Corrección

--- app/infrastructure/model/vote.py
+++ app/infrastructure/model/vote.py
@@ -6,5 +6,5 @@ class Vote(SQLModel, table=True):
     __tablename__ = "votes"
     # __table_args__ = {"schema": "public"}
 
-    post_id: int | None = Field(sa_column=Column(Integer, ForeignKey("public.posts.id",name="fk_votes_posts_id",ondelete="CASCADE",onupdate="CASCADE"), nullable=True,primary_key=True))
-    user_id: int | None = Field(sa_column=Column(Integer, ForeignKey("public.users.id",name="fk_votes_users_id",ondelete="CASCADE",onupdate="CASCADE"), nullable=True,primary_key=True))
+    post_id: int | None = Field(sa_column=Column(Integer, ForeignKey("posts.id",name="fk_votes_posts_id",ondelete="CASCADE",onupdate="CASCADE"), nullable=True,primary_key=True))
+    user_id: int | None = Field(sa_column=Column(Integer, ForeignKey("users.id",name="fk_votes_users_id",ondelete="CASCADE",onupdate="CASCADE"), nullable=True,primary_key=True))
